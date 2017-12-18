6c22664d8fbd8f83650996985cde4d9119b43e5a
Goolsby <claire.goolsby@intel.com>
2017-12-17 20:04:13 -0800

    calculates wand vector

--- wand.py
+++ wand.py
@@ -1,9 +1,11 @@
 import math
 import time
 sensor_pos = {}
+threshold = None
 
 def calculate_pointing(name, position):
     global sensor_pos
+    global threshold
     sensor_pos[name] = position
 
     if len(sensor_pos) != 3:
@@ -15,13 +17,18 @@ def calculate_pointing(name, position):
 
     # calculate distance
     wrist_dist = distance(left_wrist_pos, right_wrist_pos)
+    print(name)
 
-    if wrist_dist <= 0.2000 and time.time() >= 0.50:
+    if wrist_dist > 0.2000:
+        threshold = None
+    elif wrist_dist <= 0.2000 and threshold and time.time() > threshold:
         wrist_avg = ((left_wrist_pos[0] + right_wrist_pos[0]) / 2, ((left_wrist_pos[1] + right_wrist_pos[1]) / 2), ((left_wrist_pos[2] + right_wrist_pos[2]) / 2))
         wand_vect = (wand_pos[0] - wrist_avg[0], wand_pos[1] - wrist_avg[1], wand_pos[2] - wrist_avg[2])
-        # if wand_vect != None:
-        #    print (wand_vect)
+        #if wand_vect != None:
+        print (wand_vect)
         return wand_vect
+    elif wrist_dist <= 0.2000 and not threshold:
+        threshold = time.time() + 1.0
 
 def distance(p1, p2):
     return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2)
