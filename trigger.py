adfb07423bb666d41d16848027b152591fb68b8d
Phillip Sitbon <phillip.sitbon@intel.com>
2017-12-18 12:01:22 -0800

    dphony: fix trigger bug
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

--- trigger.py
+++ trigger.py
@@ -31,7 +31,7 @@ def velocity_update(serial, position):
     prev_pos[serial] = position
     prev_ts[serial] = now
 
-    lowpass_o1[serial] = lowpass_o1[serial] * 0.6 * velocity + 0.4
+    lowpass_o1[serial] = lowpass_o1[serial] * 0.6 * velocity * 0.4
     lowpass_o2[serial] = lowpass_o2[serial] * 0.6 + lowpass_o1[serial] * 0.4
 
     return velocity, lowpass_o2[serial]
